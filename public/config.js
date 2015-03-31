4d07f396c30e745851209d6ea30f018b096da42f
Enrique Tamames <enrique@layergloss.com>
2015-03-31 17:21:06 +0200

    Refs #5: One scroll page using angularJS

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'enrtama';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'duScroll'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,24 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core')
+	.value('duScrollDuration', 500)
+	.value('duScrollOffset', 30)
+	.controller('HomeController', ['$scope', '$document', 'Authentication',
+	function($scope, $document, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		var container = angular.element(document.querySelector('#container'));
+		var section2 = angular.element(document.querySelector('#section-2'));
+
+		$scope.toTheTop = function() {
+			container.scrollTop(0, 5000);
+		};
+
+		$scope.toSection2 = function() {
+			container.scrollTo(section2, 0, 1000);
+		};
+
 	}
 ]);
