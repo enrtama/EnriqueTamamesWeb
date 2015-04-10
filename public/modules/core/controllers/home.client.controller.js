a7651f699cc70eaaa0f6c8187863f932d6e0289a
Enrique Tamames <enrique@layergloss.com>
2015-04-10 11:55:45 +0200

    Implement some angular-ui examples: Modal + alerts

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,16 +1,20 @@
 'use strict';
 
-
 angular.module('core')
 	.value('duScrollDuration', 500)
 	.value('duScrollOffset', 30)
-	.controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+	.controller('HomeController', ['$scope', 'Authentication', '$modal', '$log',
+	function($scope, Authentication, $modal, $log) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.items = ['item1', 'item2', 'item3'];
 		$scope.container = angular.element(document.querySelector('#rightColumn'));
 		$scope.sections = angular.element($('.section').css('height', window.screen.availHeight));
 		$scope.myInterval = 2000;
+		$scope.alerts = [
+			{ type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+			{ type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+		];
 
 		$scope.toTheTop = function() {
 			$scope.container.scrollTop(0, 500);
@@ -28,5 +32,39 @@ angular.module('core')
 		for (var i = 0; i < 4; i++) {
 			$scope.addSlide();
 		}
+
+		// Example actions
+		$scope.showMessage = function() {
+			alert('Clicked!');
+  		};
+
+  		// Alerts
+		$scope.addAlert = function() {
+			$scope.alerts.push({msg: 'Another alert!'});
+		};
+		$scope.closeAlert = function(index) {
+			$scope.alerts.splice(index, 1);
+		};
+
+		// Modal
+
+		$scope.open = function (size) {
+			var modalInstance = $modal.open({
+				templateUrl: 'modules/core/views/modal.client.view.html',
+				controller: 'ModalInstanceController',
+				size: size,
+				resolve: {
+					items: function () {
+						return $scope.items;
+					}
+				}
+			});
+
+			modalInstance.result.then(function (selectedItem) {
+				$scope.selected = selectedItem;
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
 	}
 ]);
---
+++ public/modules/core/controllers/modal.client.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('core')
+	.controller('ModalInstanceController', ['$scope', '$modalInstance', 'items', 
+	function($scope, $modalInstance, items) {
+
+		$scope.items = items;
+		$scope.selected = {
+			item: $scope.items[0]
+		};
+
+		$scope.ok = function () {
+			$modalInstance.close($scope.selected.item);
+		};
+
+		$scope.cancel = function () {
+			$modalInstance.dismiss('cancel');
+		};
+
+	}
+]);
