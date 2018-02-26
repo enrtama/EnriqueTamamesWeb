24956dd98e132422dd57fa153e4fc1421c6f8af7
Enrique Tamames <enrique@layergloss.com>
2018-02-26 16:51:35 +0100

    Integrate heroku with circleci (III)

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -e
-APP_NAME=$1
-
-git remote add heroku git@heroku.com:$APP_NAME.git
-git fetch heroku
-
-PREV_WORKERS=$(heroku ps --app $APP_NAME | grep "^worker." | wc -l | tr -d ' ')
 
 # deploy code changes (and implicitly restart the app and any running workers)
-git push heroku $CIRCLE_SHA1:refs/heads/master
-
-heroku maintenance:off --app $APP_NAME
+git push heroku master
